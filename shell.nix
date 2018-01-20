a8cf6a620cad7a725556191f29d18a9b911780db
Alexey Lebedeff <alexey.lebedeff@booking.com>
2018-01-20 21:34:48 +0100

    Get secrets from environment

--- shell.nix
+++ shell.nix
@@ -8,6 +8,8 @@ with pkgs;
     hardeningDisable = ["all"];
     shellHook = ''
       export IDF_PATH=/home/binarin/personal-workspace/esp-idf/
+      export WIFI_PASSWORD=$(cat ~/.config/wifi_password)
+      export MQTT_PASSWORD=$(cat ~/.config/mqtt_password)
     '';
   };
 }
